c34f67228ffb2bfe8976abffd9d388ae65a020f6
chareef <howlong53@gmail.com>
2017-09-10 15:14:05 +0700

    12

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit,EventEmitter,Output ,Input } from '@angular/core';
-
+import {  FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-	@Output() emitCloseRegis = new EventEmitter<any>();
-  	constructor() { }
+  	rFrom:FormGroup;
+	  @Output() emitCloseRegis = new EventEmitter<any>();
+  	constructor(private fb:FormBuilder) 
+  	{ 
+  		this.rFrom = this.fb.group({
+  			'email':[null, Validators.compose([Validators.required,this.test])],
+  			'pass':[null, Validators.required ],
+  		});
+  	}
 
+    test(a)
+    {
+      if(a.value != 'reef')
+      {
+        return {'email':true}
+      }
+
+    }
   	hideRegis()
   	{
   		this.emitCloseRegis.emit(true)
   	}
 
- 	ngOnInit() 
- 	{
-  	
+    onSubmit()
+    {
+      console.log(132);
+    }
+
+
+ 	  ngOnInit() 
+ 	  {
   	}
 
 }
